a3fc3d0afbdd7c87cac3b613be3bfcda09f0559e
William Hogman <me@whn.se>
2012-04-07 01:20:35 +0200

    ENH Check if ColorCAL has meta-data before patching it

--- psychopy/hardware/crs/__init__.py
+++ psychopy/hardware/crs/__init__.py
@@ -5,6 +5,10 @@ try:
 except ImportError:
     logging.warning("Couldn't import pycrsltd. ColorCAL will not be available")
 else:
-    # Monkey-patch our metadata into their class.
-    setattr(ColorCAL,"longName","CRS ColorCAL")
-    setattr(ColorCAL,"driverFor",["colorcal"])
+    if not hasattr(ColorCAL,"longName"):
+        logging.warning("You are using an outdated versions of pycrsltd, "
+                        "consider updating to a newer version.")
+
+        # Monkey-patch our metadata into their class.
+        setattr(ColorCAL,"longName","CRS ColorCAL")
+        setattr(ColorCAL,"driverFor",["colorcal"])
